dbd88603567561fb7416aa9d61a502c73ed46f6a
Ben Stewart <ben+git@tucuxi.org>
2016-09-18 22:38:14 -0700

    Add CPR encoding functionality.

---
+++ cpr.js
@@ -0,0 +1,59 @@
+/** Utility functions to decode the Compact Position Reporting format.
+ *
+ * CPR uses a latitude-aware mapping to better optimise the use of bits
+ */
+
+
+var NUM_BITS_AIRBORNE = 17;
+var NUM_BITS_SURFACE = 19;
+var NUM_BITS_TCP = 14;
+
+// Defines the unambiguous range for decoding. For example, given that
+// the distance from the equator to the poles is 5410 nautical miles, having
+// 15 zones means that the unambiguous range is 360nm.
+var NUM_LATITUDE_ZONES = 15;
+
+// The number of longitude zones varies from equator to pole, and ranges from
+// 1 to 59 for CPR in ADS-B. This function calculates the number of zones,
+// given a latitude.
+function NL(latitude) {
+    var nz = 15;
+    var nl = Math.floor(
+        2 * Math.PI / Math.acos(
+        1 - (
+            (1 - Math.cos(0.5 * Math.PI / nz)) 
+              / 
+            Math.pow(Math.cos(Math.PI / 180 * Math.abs(latitude)), 2))));
+
+    if (isNaN(nl)) return 1;
+    return Math.min(59, Math.max(1, nl));
+}
+
+// A compact position representation consists of two different
+// encodings; an odd and an even encoding which alter the number
+// of zones in both axes. The position representation only refers
+// to the position with the local zone, which is roughly 360 nm
+// by 360 nm, except at the poles.
+//
+// Using N and N-1 zones allows the receiver to unambiguously
+// determine the global position based on a pair of odd and even
+// position reports that are received for the same zone.
+// 
+// Specifically, the difference between the odd and even CPR for
+// the same coordinate ranges from 0 to 2^bits from 0 to 90 deg.
+// 
+// If the rough location of the vehicle is known, to the
+// precision of the CPR zone, either position report is
+// sufficient to determine the global coordinate.
+function EncodeCPR(lat, lng, is_odd, bits) {
+    var i = is_odd ? 1 : 0;
+    var zone_lat_height = 90.0 / (NUM_LATITUDE_ZONES - i);
+    var zone_lng_width = 360.0 / Math.max(1, NL(lat) - i);
+
+    var xz = Math.floor(0.5 + Math.pow(2, bits) *
+                        (lng % zone_lng_width) / zone_lng_width);
+    var yz = Math.floor(0.5 + Math.pow(2, bits) *
+                        (lat % zone_lat_height) / zone_lat_height);
+
+    return {lng: xz, lat: yz};
+}
---
+++ cpr_test.js
@@ -0,0 +1,59 @@
+var NL_TEST_VECTORS = [
+    {lat: 0, nl: 59},
+    {lat: -0, nl: 59},
+    {lat: 1, nl: 59},
+    {lat: -1, nl: 59},
+    {lat: 10, nl: 59},
+    {lat: -10, nl: 59},
+    {lat: 11, nl: 58},
+    {lat: -11, nl: 58},
+    {lat: 15, nl: 57},
+    {lat: -15, nl: 57},
+    {lat: 25, nl: 54},
+    {lat: -25, nl: 54},
+    {lat: 40, nl: 45},
+    {lat: -40, nl: 45},
+    {lat: 43, nl: 43},
+    {lat: -43, nl: 43},
+    {lat: 60, nl: 29},
+    {lat: -60, nl: 29},
+    {lat: 80, nl: 10},
+    {lat: -80, nl: 10},
+    {lat: 90, nl: 1},
+    {lat: -90, nl: 1},
+
+    /* Test bad inputs - wraparound. */
+    {lat: 120, nl: 29},
+    {lat: -120, nl: 29},
+    {lat: 137, nl: 43},
+    {lat: -137, nl: 43},
+];
+
+var AIRBORNE = NUM_BITS_AIRBORNE;
+
+var CPR_TEST_VECTORS = [
+    {lat: 0, lng: 0, bits: AIRBORNE, is_odd: false, cpr_lat: 0, cpr_lng: 0},
+    {lat: 0, lng: 0, bits: AIRBORNE, is_odd: true,  cpr_lat: 0, cpr_lng: 0},
+    // Test that almost-90 degrees is the maximal value.
+    {lat: 89.99995, lng: 0, bits: AIRBORNE, is_odd: false,
+     cpr_lat: 131071, cpr_lng: 0},
+    {lat: 89.99995, lng: 0, bits: AIRBORNE, is_odd: true,
+     cpr_lat: 131071, cpr_lng: 0},
+    {lat: 90, lng: 0, bits: AIRBORNE, is_odd: false, cpr_lat: 0, cpr_lng: 0},
+    {lat: 90, lng: 0, bits: AIRBORNE, is_odd: true,  cpr_lat: 0, cpr_lng: 0},
+];
+
+testThat('Calculates correct number of longitude zones', NL_TEST_VECTORS.every(
+    function(input) {
+        return assertEqual('NL(' + input.lat + ')', input.nl, NL(input.lat));
+    }));
+
+testThat('Calculates CPR for coordinates', CPR_TEST_VECTORS.every(
+    function(input) {
+        var output = EncodeCPR(input.lat, input.lng, input.is_odd, input.bits);
+        var call = ('EncodeCPR(' + input.lat + ', ' + input.lng + ', ' +
+                    input.is_odd + ', ' + input.bits + ')');
+        return (
+            assertEqual(call + '.lat', input.cpr_lat, output.lat) &&
+            assertEqual(call + '.lng', input.cpr_lng, output.lng));
+    }));
