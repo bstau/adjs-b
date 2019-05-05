39d02aff9d741affcde1c10ff4ca61f3d1cd9011
Ben Stewart <git@tucuxi.org>
2019-05-05 16:23:49 -0700

    Add test cases for Phillipines, Ukraine

--- address_test.js
+++ address_test.js
@@ -287,8 +287,9 @@ const EXPECTED_TAIL_NUMBERS = [
     {icao: 0x47C5C9, country: 'NO'},
     {icao: 0x484006, country: 'NL'},
     {icao: 0x48456F, country: 'NL'},
+    {icao: 0x75847C, country: 'PH'},
     {icao: 0x89901A, country: 'TW'},
-    {icao: 0x50836C, country: 'UK'},
+    {icao: 0x50836C, country: 'UA'},
 ];
 
 testThat('Determines correct country code from ICAO addresses', EXPECTED_TAIL_NUMBERS.every(
